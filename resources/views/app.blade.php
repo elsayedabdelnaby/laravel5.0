d1b1dd35ba9471a0138735911fcf6e353cb71252
elsayed abd elnaby <elsayed.fcis.1@gmail.com>
2017-10-03 22:39:49 +0200

    create a master page and extends it on about view[F

---
+++ resources/views/app.blade.php
@@ -0,0 +1,12 @@
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Document</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	</head>
+	<body>
+		<div class="container">
+			@yield('content')
+		</div>
+	</body>
+</html>
--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,12 +1,8 @@
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
-</head>
-<body>
+@extends('app')
+
+@section('content')
     <h1>About Me: {!! $name !!}</h1>
     <p>
     	{!! $job !!}
     </p>
-</body>
-</html>
+@stop
