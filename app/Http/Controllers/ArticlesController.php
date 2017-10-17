f24e3a1dea8338e046a24b8c8e5108dff3260a97
elsayed abd elnaby <elsayed.fcis.1@gmail.com>
2017-10-18 00:03:40 +0200

    passing any date time column to latest function to make order by it

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -10,7 +10,7 @@ use Illuminate\Http\Request;
 class ArticlesController extends Controller {
 
 	public function index(){
-		$articles = Article::orderBy('published_at', 'desc')->get();
+		$articles = Article::latest('published_at')->get();
 		return view('articles.index', compact('articles'));
 	}
 
